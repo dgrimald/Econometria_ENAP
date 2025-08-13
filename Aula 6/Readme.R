ca77eafa50c55f0dbd7309aea87bf2daa6a089c8
Arthur Bragança <arthurbraganca@gmail.com>
2025-08-13 17:44:02 -0300

    lista

---
+++ Aula 6/Readme.R
@@ -0,0 +1,19 @@
+grape: escolha inicial de uvas
+dg: mudança na escolha de uvas
+incentive: 1 = se aluno recebeu incentivos
+private: 1 = se escolha era privada
+table_incentive_pr: porcentagem das crianças que recebe incentivos
+
+Controles (incluir como variáveis categóricas com opção as.factor()):
+  
+  strata: estrato
+  sex: gênero da criança
+  race: raça 
+  lunch: se tem subsídios para almoço 
+  table_size: tamanho da mesa
+  grade: série
+  
+Cluster: variável para clusterizar erro-padrão
+
+
+  
